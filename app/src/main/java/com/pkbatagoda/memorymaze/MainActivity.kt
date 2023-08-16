f99c423306057d18ee7e7b1d885400df59babf1d
Pasindu Batagoda <pasindubatagoda@gmail.com>
2023-08-16 16:12:46 +0530

    create main layout

--- app/src/main/java/com/pkbatagoda/memorymaze/MainActivity.kt
+++ app/src/main/java/com/pkbatagoda/memorymaze/MainActivity.kt
@@ -2,10 +2,22 @@ package com.pkbatagoda.memorymaze
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    // referencing widgets in layout
+    private lateinit var rvBoard : RecyclerView
+    private lateinit var tvNumMoves : TextView
+    private lateinit var tvNumPairs : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set the newly defined variables equal to the corresponding view in the layout
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
     }
 }
