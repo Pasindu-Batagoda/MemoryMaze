75e1ffdc9b612204ec62a10faa3874fc34880307
Pasindu Batagoda <pasindubatagoda@gmail.com>
2023-08-17 15:42:00 +0530

    setup recycler view, show memory cards

--- app/src/main/java/com/pkbatagoda/memorymaze/MainActivity.kt
+++ app/src/main/java/com/pkbatagoda/memorymaze/MainActivity.kt
@@ -3,7 +3,9 @@ package com.pkbatagoda.memorymaze
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +21,13 @@ class MainActivity : AppCompatActivity() {
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+
+        rvBoard.setHasFixedSize(true)
+        // spanCount - number of columns in the Recycler view
+        rvBoard.layoutManager = GridLayoutManager (this , 2)
+
+
     }
 }
