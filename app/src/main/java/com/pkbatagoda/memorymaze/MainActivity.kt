41b378de1d4ea05520597b6ceb914d6cb73f9881
Pasindu Batagoda <pasindubatagoda@gmail.com>
2023-08-23 20:04:27 +0530

    display game information

--- app/src/main/java/com/pkbatagoda/memorymaze/MainActivity.kt
+++ app/src/main/java/com/pkbatagoda/memorymaze/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.pkbatagoda.memorymaze
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
@@ -15,17 +17,16 @@ import com.pkbatagoda.memorymaze.utils.DEFAULT_ICONS
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         private const val TAG = "mainActivity"
     }
 
 
-
     // referencing widgets in layout
-    private lateinit var clRoot : ConstraintLayout
-    private lateinit var rvBoard : RecyclerView
-    private lateinit var tvNumMoves : TextView
-    private lateinit var tvNumPairs : TextView
+    private lateinit var clRoot: ConstraintLayout
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
 
     private lateinit var memoryGame: MemoryGame
     private lateinit var adapter: MemoryBoardAdapter
@@ -40,20 +41,25 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-       memoryGame = MemoryGame(boardSize)
+        tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
+        memoryGame = MemoryGame(boardSize)
 
-        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
-            override fun onCardClicked(position: Int) {
-                updateGameWithFlip(position)
-            }
+        adapter = MemoryBoardAdapter(
+            this,
+            boardSize,
+            memoryGame.cards,
+            object : MemoryBoardAdapter.CardClickListener {
+                override fun onCardClicked(position: Int) {
+                    updateGameWithFlip(position)
+                }
 
-        })
+            })
 
         rvBoard.adapter = adapter
 
         rvBoard.setHasFixedSize(true)
         // spanCount - number of columns in the Recycler view
-        rvBoard.layoutManager = GridLayoutManager (this , boardSize.getWidth())
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
 
 
     }
@@ -61,23 +67,30 @@ class MainActivity : AppCompatActivity() {
     // update the memory game with an attempted flip at the specific position
     private fun updateGameWithFlip(position: Int) {
         // error checking
-        if(memoryGame.haveWonGame()){
+        if (memoryGame.haveWonGame()) {
             Snackbar.make(clRoot, "You already won!", Snackbar.LENGTH_LONG).show()
             return
         }
-        if(memoryGame.isCardFaceUp(position)){
+        if (memoryGame.isCardFaceUp(position)) {
             Snackbar.make(clRoot, "Invalid move!", Snackbar.LENGTH_SHORT).show()
             return
         }
         // actually flipping over the card
-        if (memoryGame.flipCard(position)){
+        if (memoryGame.flipCard(position)) {
             Log.i(TAG, "Found a match! num pairs found: ${memoryGame.numPairsFound}")
-            tvNumPairs.text = "pairs: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()}"
-            if (memoryGame.haveWonGame()){
+
+            val color = ArgbEvaluator().evaluate(
+                memoryGame.numPairsFound.toFloat() / boardSize.getNumPairs(),
+                ContextCompat.getColor(this, R.color.color_progress_none),
+                ContextCompat.getColor(this, R.color.color_progress_full)
+            ) as Int
+            tvNumPairs.setTextColor(color)
+            tvNumPairs.text = "Pairs: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()}"
+            if (memoryGame.haveWonGame()) {
                 Snackbar.make(clRoot, "You won! congratulations.", Snackbar.LENGTH_LONG).show()
             }
         }
-        tvNumMoves.text = "Moves: ${memoryGame}"
+        tvNumMoves.text = "Moves: ${memoryGame.getNumMoves()}"
         adapter.notifyDataSetChanged()
 
     }
--- app/src/main/java/com/pkbatagoda/memorymaze/models/MemoryGame.kt
+++ app/src/main/java/com/pkbatagoda/memorymaze/models/MemoryGame.kt
@@ -8,6 +8,9 @@ class MemoryGame (private val boardSize: BoardSize){
     val cards: List<MemoryCard>
     var numPairsFound = 0
 
+
+
+    private var numCardFlips = 0
     private var indexOfSingleSelectedCard: Int? = null
     init {
         val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
@@ -16,6 +19,7 @@ class MemoryGame (private val boardSize: BoardSize){
     }
 
     fun flipCard(position: Int): Boolean {
+        numCardFlips ++
         val card : MemoryCard = cards[position]
 
         var foundMatch = false
@@ -56,5 +60,9 @@ class MemoryGame (private val boardSize: BoardSize){
 
     fun isCardFaceUp(position: Int): Boolean {
         return cards[position].isFaceUp
+    }
+
+    fun getNumMoves(): Int {
+        return numCardFlips / 2
     }
 }
